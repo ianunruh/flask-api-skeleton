ac879e0aa214f09929bfe79ac1a10c4d6b1a16d2
Ian Unruh <ianunruh@gmail.com>
2016-09-17 19:00:40 -0700

    Strip equals in token generator

--- backend/util.py
+++ backend/util.py
@@ -4,4 +4,4 @@ import os
 
 def generate_token(nbytes=32):
     raw = os.urandom(nbytes)
-    return base64.urlsafe_b64encode(raw).decode('utf-8')
+    return base64.urlsafe_b64encode(raw).decode('utf-8').rstrip('=')
